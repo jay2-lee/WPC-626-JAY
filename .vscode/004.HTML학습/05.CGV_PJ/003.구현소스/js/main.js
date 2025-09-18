6d20434365c8589e54f621ba60fb5fb2dfb390d5
jay <jhgh20201007@gmail.com>
2025-09-18 10:03:23 +0900

    18

--- .vscode/004.HTML학습/05.CGV_PJ/003.구현소스/js/main.js
+++ .vscode/004.HTML학습/05.CGV_PJ/003.구현소스/js/main.js
@@ -2,3 +2,11 @@
 
 // 로딩확인
 console.log("나야나 로딩!");
+
+
+// 3. 클릭된 a요소 자식중 h2요소의 글자읽기
+let 영화명 = 요소.querySelector("h2").innerText;
+console.log("클릭된 영화명", 영화명);
+
+// 4. 아이프레임 소스 변경하기
+아이프레임.src = `https://www.youtube.com/embed/&{유튜브{영화명}}?autoplay=1`;
