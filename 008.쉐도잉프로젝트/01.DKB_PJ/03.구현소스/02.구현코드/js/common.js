abe662d87ef9ef3a550c3ad35790c3e6a89f02ab
jay <jhgh20201007@gmail.com>
2025-11-04 09:45:22 +0900

    564

--- 008.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/02.구현코드/js/common.js
+++ 008.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/02.구현코드/js/common.js
@@ -40,6 +40,30 @@ import bannerFn from "./main1_jquery.js";
 
 /// 2. 상단파트에서 실행할 함수 /////////////
 function headerFn() {
+  /***************************** 
+    JS 로 링크 시스템 만들기
+ *****************************/
+  // 대상 : 상단영역 a요소
+  const $topLink = $("#top-area a");
+  $topLink.click(function (e) {
+    // e - 이벤트 객체
+    // 기본이동 막기
+    e.preventDefault();
+    // 클릭된 a요소의 글자 읽기
+    let aTxt = $(this).text();
+    console.log(aTxt);
+    // 분기하여 이동
+    // 이동은 location.href = '링크주소';
+    switch (aTxt) {
+      case "로그인":
+        location.href = "login.html";
+        break;
+      case "회원가입":
+        location.href = "member.html";
+        break;
+    } //// switch문 //////////
+  }); /////// click 이벤트 //////////////
+
   ///////////////////////////////////////////////////
   // 1. 큐브로고박스 일정간격으로 클래스 넣었다 빼기 ///
   //////////////////////////////////////////////////
@@ -131,4 +155,67 @@ function headerFn() {
     // 메뉴박스 z-index:0처리
     $menuBox.css("z-index", "0");
   }); /// click ///
+
+  // 코드의 지역화 //////
+  // (()=>{})()
+
+  ////////// 스크롤시 상단영역 방향별 보이기/숨기기 //////
+  (() => {
+    // 변경대상 : 상단영역 .header
+    const header = document.querySelector("#top-area");
+    const stkMenu = document.querySelector("#spart-menu");
+
+    // 스티키 셋팅값
+    let stkValue = 122;
+
+    // 가로크기를 체크하여 stkValue값을 변경하는 함수 만들기
+    const checkWidth = () => {
+      // 윈도우 가로크기 읽어오기 : window.innerWidth
+      // 만약 가로크기가 1024이하이면 크기값을 60으로 변경
+      window.innerWidth <= 1024 ? (stkValue = 60) : (stkValue = 122);
+    }; /////////// checkWidth 함수 //////////
+
+    // 처음로딩시 가로크기 체크함수 호출
+    checkWidth();
+    // 화면크기 변경시(resize이벤트) 가로크기 체크함수 호출
+    window.addEventListener("resize", checkWidth);
+    // resize이벤트는 브라우저 윈도우화면크기를 변경할때마다 발생함!
+
+
+    // 스크롤 방향 알아내는 원리:
+    // (1) 아랫방향
+    // 이전 스크롤위치값 < 현재 스크롤위치값
+    // (2) 윗방향
+    // 이전 스크롤위치값 > 현재 스크롤위치값
+
+    // 이전 스크롤 위치값 저장변수
+    let prevScroll = 0;
+
+    // 스크롤 이벤트 설정하기
+    window.addEventListener("scroll", () => {
+      // 스크롤 위치값 구하기
+      let curScroll = window.scrollY;
+      // console.log('스크롤~~~~!', curScroll);
+
+      // (1) 아랫방향
+      // 이전 스크롤위치값 < 현재 스크롤위치값
+      if (prevScroll < curScroll) {
+        // console.log("스크롤 내려간다~~!");
+        // 스크롤 내려가면 메뉴 숨기기
+        header.classList.add("hide");
+        stkMenu.style.top = "0px";
+      } /// if ////
+      // (2) 윗방향 : 아랫방향이 아니면 윗방향
+      // 이전 스크롤위치값 > 현재 스크롤위치값
+      else {
+        // console.log("스크롤 올라간다~~!");
+        // 스크롤 올라가면 메뉴 보이기
+        header.classList.remove("hide");
+        stkMenu.style.top = stkValue + "px";
+      } /// else ////
+
+      // 중요!!! 마지막에 이전스크롤위치를 저장!
+      prevScroll = curScroll;
+    }); /////////// scroll ///////////////
+  })(); /// 스크롤시 상단영역 방향별 보이기/숨기기 //////
 } ////////////// headerFn ///////////////
